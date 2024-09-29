6ad47cdb9c450637869b48da7edfcae0239362fd
VISHALSP969 <vishalsp969@gmail.com>
2024-09-29 18:09:16 +0530

    Python variables

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Creating variables
+# Integer
+a = 2
+print(a)
+
+b = 987654321987654321987654321
+print(b)
+
+# Floating point
+pi = 3.14
+print(pi)
+
+# String
+c = 'A'
+print(c)
+
+name = 'John Doe'
+print(name)
+
+# Boolean
+q = True
+print(q)
+
+# Empty value or null datatype
+x = None
+print(x)
