c35ebc2e7a72acb7fc4d81d0051f71ed2ef66ba2
VISHALSP969 <vishalsp969@gmail.com>
2024-09-29 18:38:41 +0530

    Variable naming rules

---
+++ variable_naming.py
@@ -0,0 +1,21 @@
+# Variable naming rules
+
+# Starts with a letter or an underscore
+x = 10
+_x = 100
+print(x, _x)
+
+# 9x=10     #invalid
+# $y=100    #Invalid
+
+has_0_in_it = 1000
+print(has_0_in_it)
+
+# Names are case sensitive
+a = 20
+A = 200
+print(a, A)
+
+
+p=9
+# q=P*10      #Name error
