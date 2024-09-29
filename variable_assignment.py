073078ff20cb55673156e7f004bbc4c62f4349d5
VISHALSP969 <vishalsp969@gmail.com>
2024-09-29 19:53:00 +0530

    Variable assignments

---
+++ variable_assignment.py
@@ -0,0 +1,26 @@
+# Variable assignment
+a = 10
+print(a)
+
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+# x,y,z=1,2     #Value Error
+# x,y=1,2,3       #Value Error
+
+p, q, _ = 10, 20, 30
+print(p, q)
+
+j = k = l = 100
+print(j, k, l)
+
+x = y = [7, 8, 9]
+x = [13, 8, 9]
+print(y)
+
+x = y = [7, 8, 9]
+print(x, y)
+print(x[0])
+
+b=[1,2,3,[4,5],6]
+print(b)
