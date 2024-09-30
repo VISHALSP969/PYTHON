4729194e681deaebcee174a188c97b5e0f56980b
VISHALSP969 <vishalsp969@gmail.com>
2024-09-30 18:33:28 +0530

    String datatype

---
+++ PythonDatatypes/strings.py
@@ -0,0 +1,18 @@
+mystring = "This is my string"
+print(mystring)
+mystring1 = 'This is another string'
+print(mystring1)
+
+str1 = "Hello"
+print(id(str1))
+str1 = "bye"
+print(id(str1))
+
+str2 = "MAP"
+print(id(str2))
+print(str2[2])
+# str2[2] = 'P'     # Type error
+
+a_str = 'Hello world'
+print(a_str[0])
+print(a_str[0:5])       # slice operator
