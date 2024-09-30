c669eea11f5ace555464c44a7dbc35ed53d16af3
VISHALSP969 <vishalsp969@gmail.com>
2024-09-30 15:08:14 +0530

    User input

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# input() function
+
+name = input('Enter your name : ')
+print(name)
+
+x = input('Enter a number : ')
+print(x)
+# print(x/2)      # Type error
+print(int(x)/2)
+print(float(x)/2)
