46771d5a1c8959cd94472daebb67251459909fe9
VISHALSP969 <vishalsp969@gmail.com>
2024-09-30 19:50:54 +0530

    Python sets

---
+++ PythonDatatypes/sets.py
@@ -0,0 +1,15 @@
+# Set
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+
+a = set('abracadabra')
+print(a)
+
+a.add('z')
+print(a)
+
+# Frozen set
+b = frozenset('asdfagsa')
+print(b)
+cities = frozenset(["Frankfurt", "Basel", "Freiburg"])
+print(cities)
