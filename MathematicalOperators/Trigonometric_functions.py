2f51c5c7ca9ef6fb3cd223a198a8baea4bd9450e
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 11:37:11 +0530

    Trigonometric functions

---
+++ MathematicalOperators/Trigonometric_functions.py
@@ -0,0 +1,15 @@
+import math
+
+a, b = 1, 2
+
+print(math.sin(a))
+print(math.cos(b))
+
+print(math.atan(math.pi))
+
+print(math.hypot(a, b))
+
+# convert radians to degrees
+print(math.degrees(a))
+# convert degrees to radians
+print(math.radians(57.29577951308232))
