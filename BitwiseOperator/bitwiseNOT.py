ec8856caf5e0ca2600e8c1289d801b63ee6900c3
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 15:44:49 +0530

    Bitwise NOT

---
+++ BitwiseOperator/bitwiseNOT.py
@@ -0,0 +1,10 @@
+print(~0)
+print(~1)
+print(~2)
+print(~3)
+print(~4)
+
+print(~-1)
+print(~-2)
+print(~-3)
+print(~-4)
--- MathematicalOperators/modulus.py
+++ MathematicalOperators/modulus.py
@@ -1,16 +1,16 @@
 import operator
 
-print(3%4)
-print(10%2)
-print(6%4)
+print(3 % 4)
+print(10 % 2)
+print(6 % 4)
 
-print(operator.mod(3,4))
-print(operator.mod(10,2))
-print(operator.mod(6,4))
+print(operator.mod(3, 4))
+print(operator.mod(10, 2))
+print(operator.mod(6, 4))
 
-print(-9%7)
-print(9%-7)
-print(-9%-7)
+print(-9 % 7)
+print(9 % -7)
+print(-9 % -7)
 
-quotient,remainder=divmod(9,4)
-print(quotient,remainder)
+quotient, remainder = divmod(9, 4)
+print(quotient, remainder)
