c88b813ecfc6e5ab7dcb951f035c59cb3a0c1694
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 16:11:00 +0530

    Bitwise right shift

---
+++ BitwiseOperator/bitwiseRightShift.py
@@ -0,0 +1,17 @@
+a = 10
+print(bin(a))
+print(a >> 1)
+print(bin(a >> 1))
+print(a >> 2)
+print(bin(a >> 2))
+print(a >> 3)
+print(bin(a >> 3))
+
+b = -10
+print(bin(b))
+print(b >> 1)
+print(bin(b >> 1))
+print(b >> 2)
+print(bin(b >> 2))
+print(b >> 3)
+print(bin(b >> 3))
