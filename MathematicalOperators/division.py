3b58e4be69773c28f2372d1858b1106692abf15f
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 09:32:45 +0530

    Division operartor

---
+++ MathematicalOperators/division.py
@@ -0,0 +1,17 @@
+print(10 / 2)
+print(10.0 / 2)
+print(10 / 2.0)
+print(10.0 / 2.0)
+
+print(10 / 3)
+print(10.0 / 3)
+print(10 / 3.0)
+print(10.0 / 3.0)
+
+print(10 / (3 + 2j))
+print((3 + 2j) / 10)
+
+print(10 // 3)
+print(10.0 // 3)
+print(10 // 3.0)
+print(10.0 // 3.0)
