eb8e62190c9144ba60fa2eec17b6e76d4e189bb7
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 14:04:07 +0530

    Log operators

---
+++ MathematicalOperators/logarithms.py
@@ -0,0 +1,15 @@
+import math
+import cmath
+
+print(math.log(5))
+# optional base argument, default is math.e
+print(math.log(5,math.e))
+print(math.log(1000,10))
+
+print(cmath.log(5))
+print(cmath.log(1000,10))
+
+print(math.log10(5))
+print(math.log2(8))
+print(math.log10(1000))
+print(cmath.log10(1000))
