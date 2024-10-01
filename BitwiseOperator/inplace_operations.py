98777524f5c2e7ba0cd39ddd8a950562cd3778fb
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 16:17:22 +0530

    Inplace operations

---
+++ BitwiseOperator/inplace_operations.py
@@ -0,0 +1,27 @@
+a = 1
+b = 2
+print(bin(a))
+print(bin(b))
+a &= b
+print(bin(a))
+
+a = 1
+b = 2
+print(bin(a))
+print(bin(b))
+a |= b
+print(bin(a))
+
+a = 1
+b = 2
+print(bin(a))
+print(bin(b))
+a <<= b
+print(bin(a))
+
+a = 1
+b = 2
+print(bin(a))
+print(bin(b))
+a >>= b
+print(bin(a))
