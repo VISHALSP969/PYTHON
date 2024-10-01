564d5a03f672c850bd221924fe00185e10b9c4e3
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 16:39:33 +0530

    Logical AND

---
+++ BooleanOperators/logicalAND.py
@@ -0,0 +1,39 @@
+x = True
+y = True
+z = x and y
+print(z)
+
+x = True
+y = False
+z = x and y
+print(z)
+
+x = False
+y = True
+z = x and y
+print(z)
+
+x = False
+y = False
+z = x and y
+print(z)
+
+x = 1
+y = 1
+z = x and y
+print(z)
+
+x = 1
+y = 0
+z = x and y
+print(z)
+
+x = 0
+y = 1
+z = x and y
+print(z)
+
+x = 0
+y = 0
+z = x and y
+print(z)
