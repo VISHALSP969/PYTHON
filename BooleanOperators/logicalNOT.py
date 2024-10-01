cfb26a9cf11c2ef753f298f1fe4d197b1d42424a
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 16:49:44 +0530

    Logical NOT

---
+++ BooleanOperators/logicalNOT.py
@@ -0,0 +1,23 @@
+x = True
+y = not x
+print(y)
+
+x = False
+y = not x
+print(y)
+
+str1 = "abc"
+print(not str1)
+
+str2 = ""
+print(not str2)
+
+list1 = [1, 2, 3]
+print(not list1)
+
+list2 = []
+print(not list2)
+
+value = None
+print(value)
+print(not value)
