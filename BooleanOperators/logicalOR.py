eda8a6b58807c2b233ed0c8887435e353de12d0b
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 16:42:03 +0530

    Logical OR

---
+++ BooleanOperators/logicalOR.py
@@ -0,0 +1,39 @@
+x = True
+y = True
+z = x or y
+print(z)
+
+x = True
+y = False
+z = x or y
+print(z)
+
+x = False
+y = True
+z = x or y
+print(z)
+
+x = False
+y = False
+z = x or y
+print(z)
+
+x = 1
+y = 1
+z = x or y
+print(z)
+
+x = 1
+y = 0
+z = x or y
+print(z)
+
+x = 0
+y = 1
+z = x or y
+print(z)
+
+x = 0
+y = 0
+z = x or y
+print(z)
