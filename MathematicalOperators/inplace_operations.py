a5a8d97c558667ca0521313fadcfdc7aecf24473
VISHALSP969 <vishalsp969@gmail.com>
2024-10-01 11:53:50 +0530

    Inplace operations

---
+++ MathematicalOperators/inplace_operations.py
@@ -0,0 +1,30 @@
+a = 10
+a = a + 1
+print(a)
+a = a * 2
+print(a)
+
+a = 10
+a += 1
+print(a)
+a *= 2
+print(a)
+
+a = 10
+a += 1
+print(a)
+a -= 1
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a //= 3
+print(a)
+a = 10
+a %= 3
+print(a)
+a = 2
+b = 3
+a **= b
+print(a)
