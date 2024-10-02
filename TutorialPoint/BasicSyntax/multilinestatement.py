cc1784af38ff6ebf7dc40603827adfc0dc12d960
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 03:14:50 +0530

    MULTI LINE STATEMENTS

---
+++ TutorialPoint/BasicSyntax/multilinestatement.py
@@ -0,0 +1,19 @@
+item_one = 10
+item_two = 20
+item_three = 30
+result = item_one + \
+         item_two + \
+         item_three
+print(result)
+
+list1 = [1, 2, 3,
+         4, 5]
+print(list1)
+
+tuple1=(1,2,3,
+        4,5)
+print(tuple1)
+
+set1={1,2,3,
+      4,5}
+print(set1)
