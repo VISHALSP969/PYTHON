4271ed47cec11229ee192ae16e405276afcf6aa3
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 03:53:18 +0530

    MULTIPLE STATEMENT GROUPS AS SUITES

---
+++ TutorialPoint/BasicSyntax/statements2.py
@@ -0,0 +1,21 @@
+num = 12
+if num > 10:
+    # suite
+    print("number is greater than 10")
+    print("Done")
+elif num > 5:
+    # suite
+    print("number is greater than 5")
+    print("Done")
+elif num > 0:
+    # suite
+    print("number is greater than 0")
+    print("Done")
+elif num == 0:
+    # suite
+    print("number is 0")
+    print("Done")
+else:
+    # suite
+    print("number is negative")
+    print("Done")
