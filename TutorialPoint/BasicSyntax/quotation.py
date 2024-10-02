f7d27fbe8771a48ff4056fe09a31473289244f93
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 03:20:33 +0530

    QUOTATIONS

---
+++ TutorialPoint/BasicSyntax/quotation.py
@@ -0,0 +1,10 @@
+# ' '," " and """ """ denotes string literals
+word = 'word'
+sentence = "This is a sentence"
+# triple quotes are used to span the string across multiple lines
+paragraph = """This is a paragraph.It is
+made up of multiple lines and sentences."""
+
+print(word)
+print(sentence)
+print(paragraph)
