6bab6e4c67e53f26ec9d2995ac0bf56ad73e01b4
VISHALSP969 <vishalsp969@gmail.com>
2024-10-02 20:55:18 +0530

    While loop

--- Loops/while_loop.py
+++ Loops/while_loop.py
@@ -1,7 +1,22 @@
-a=10
+import cmath
+
+a = 10
 while True:
-    a=a-1
+    a = a - 1
     print(a)
-    if a<7:
+    if a < 7:
         break
 print("Done")
+
+i = 0
+while i < 4:
+    print(i)
+    i = i + 1
+
+complex_num=cmath.sqrt(-1)
+while complex_num:
+    print(complex_num)
+    break
+
+while True:
+    print("Infinite loop")
