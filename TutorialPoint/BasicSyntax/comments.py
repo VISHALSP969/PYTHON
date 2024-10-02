13ebc6ed093e7f024ae0ebb8f8ba1db6282825d3
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 03:38:03 +0530

    COMMENTS

---
+++ TutorialPoint/BasicSyntax/comments.py
@@ -0,0 +1,9 @@
+# first comment
+print("Hello ,Python!")  # second comment
+
+name = "Madisetti"  # this is again comment
+
+# This is a comment
+# This is a comment, too.
+# This is a comment,too.
+# I said that already.
