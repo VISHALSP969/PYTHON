8bc794cee2a66115a27147c977437c418a203877
VISHALSP969 <vishalsp969@gmail.com>
2024-10-02 14:26:16 +0530

    >,<,>= and <=

---
+++ Comparisons/sample2.py
@@ -0,0 +1,14 @@
+print(12 > 4)
+print(12 < 4)
+print(1 < 4)
+
+print("alpha" < "beta")
+print("gamma" < "beta")
+print("gamma" < "OMEGA")
+
+print(5 <= 5)
+print(5 >= 5)
+print(5 <= 10)
+print(5 >= 10)
+print(10 <= 5)
+print(10 >= 5)
