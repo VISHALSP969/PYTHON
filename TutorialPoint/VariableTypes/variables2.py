458c4dbcb88aa32904a65732d319e7a225844e55
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 10:15:05 +0530

    MULTIPLE ASSIGNMENT

---
+++ TutorialPoint/VariableTypes/variables2.py
@@ -0,0 +1,11 @@
+# assign single value to several variables
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+# assign multiple objects to multiple variables
+a, b, c = 10, 20, "John"
+print(a)
+print(b)
+print(c)
