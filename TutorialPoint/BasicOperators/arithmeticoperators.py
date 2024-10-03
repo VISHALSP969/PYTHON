60b8685b52dc1eef29527f93c8fcba3dba82959e
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 12:59:58 +0530

    ARITHMETIC OPERATORS

---
+++ TutorialPoint/BasicOperators/arithmeticoperators.py
@@ -0,0 +1,26 @@
+a = 21
+b = 10
+
+c = a + b
+print(c)
+
+c = a - b
+print(c)
+
+c = a * b
+print(c)
+
+c = a / b
+print(c)
+
+c = a // b
+print(c)
+
+c = a % b
+print(c)
+
+a = 2
+b = 3
+
+c = a ** b
+print(c)
