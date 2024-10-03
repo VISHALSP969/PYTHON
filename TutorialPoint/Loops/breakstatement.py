e7b0b03afc35a522a6be10d6341f439fbeabfe14
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 20:13:45 +0530

    BREAK STATEMENT

---
+++ TutorialPoint/Loops/breakstatement.py
@@ -0,0 +1,12 @@
+for letter in 'python':
+    if letter == 'h':
+        break
+    print('Current letter', letter)
+
+var = 10
+while var > 0:
+    print('Current variable value :', var)
+    var = var - 1
+    if var == 5:
+        break
+print('Good bye!')
