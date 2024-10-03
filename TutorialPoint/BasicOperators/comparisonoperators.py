56e425381988ed9e07b0b4b7dcce6f78cbecf8bc
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 14:35:32 +0530

    COMPARISON OPERATORS

---
+++ TutorialPoint/BasicOperators/comparisonoperators.py
@@ -0,0 +1,41 @@
+a = 21
+b = 10
+c = 0
+
+if a == b:
+    print('a is equal to b')
+else:
+    print('a is mot equal to b')
+
+if a != b:
+    print('a is not equal to b')
+else:
+    print('a is equal to b')
+
+# if a<>b:      # syntax error
+#     print('a is not equal to b')
+# else:
+#     print('a is equal to b')
+
+if a < b:
+    print('a is less than b')
+else:
+    print('a is not less than b')
+
+if a > b:
+    print('a is greater than b')
+else:
+    print('a is not greater than b')
+
+a = 5
+b = 20
+
+if a <= b:
+    print('a is either less than or equal to b')
+else:
+    print('a is neither less than or equal to b')
+
+if a>=b:
+    print('b is neither greater than or equal to a')
+else:
+    print('b is either greater than or equal to a')
