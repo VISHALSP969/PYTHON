8758a1f400f33f309c3d04b1c3a0f73f1cd84aab
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 20:47:00 +0530

    PYTHON NUMBERS

---
+++ TutorialPoint/Numbers/numbers1.py
@@ -0,0 +1,17 @@
+var1 = 10
+var2 = 20
+print(var1)
+print(var2)
+del var1, var2
+# print(var1)       # Name Error
+# print(var2)       # Name Error
+
+
+int_val=10
+float_val=3.14
+complex_val=3+2j
+long_val=123456789123456789123456789123456789
+print(int_val)
+print(float_val)
+print(complex_val)
+print(long_val)
