cf5fcc3d30a2d226fa66d5802719a0f117dba50c
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 20:17:45 +0530

    CONTINUE STATEMENT

---
+++ TutorialPoint/Loops/continuestatement.py
@@ -0,0 +1,13 @@
+for letter in 'python':
+    if letter == 'h':
+        continue
+    print('Current letter :', letter)
+print('Good bye!')
+
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print('Current variable value :', var)
+print('Good bye!')
