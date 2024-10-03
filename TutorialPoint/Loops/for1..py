05dc35c8938dc0f8391e5d8683fdbcfa81c66e40
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 20:02:52 +0530

    FOR LOOP

---
+++ TutorialPoint/Loops/for1..py
@@ -0,0 +1,7 @@
+for letter in 'python':
+    print(letter)
+
+fruits = ['banana', 'apple', 'mango']
+
+for fruit in fruits:
+    print(fruit)
---
+++ TutorialPoint/Loops/for2.py
@@ -0,0 +1,4 @@
+fruits = ['apple', 'banana', 'mango']
+
+for index in range(len(fruits)):
+    print(fruits[index])
