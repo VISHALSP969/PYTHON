e55ebcc890758b5c300f412f12bf8183570f6f3a
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 23:33:17 +0530

    TRIGONOMETRIC FUNCTIONS

---
+++ TutorialPoint/Numbers/trigonometricfunctions.py
@@ -0,0 +1,35 @@
+import math
+
+angle = math.pi / 4
+
+print(math.sin(angle))
+print(math.cos(angle))
+print(math.tan(angle))
+
+# inverse trigonometric functions
+print(math.asin(0.5))
+print(math.acos(0.5))
+print(math.atan(1))
+
+# convert radians to degrees
+print(math.degrees(math.pi))
+print(math.degrees(math.pi / 2))
+print(math.degrees(math.pi / 3))
+print(math.degrees(math.pi / 4))
+
+# convert degrees to radians
+print(math.radians(0))
+print(math.radians(30))
+print(math.radians(45))
+print(math.radians(60))
+print(math.radians(90))
+print(math.radians(180))
+
+y = 1
+x = 1
+print(math.atan2(y, x))
+
+# hyperbolic
+print(math.sinh(1))
+print(math.cosh(1))
+print(math.tanh(1))
