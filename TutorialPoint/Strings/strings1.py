aba51fc4e960e5e0d2d2cde8c26ef94480ebfd78
VISHALSP969 <vishalsp969@gmail.com>
2024-10-04 01:45:02 +0530

    PYTHON STRINGS

---
+++ TutorialPoint/Strings/strings1.py
@@ -0,0 +1,8 @@
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print(var1)
+print(var2)
+
+print(var1[0])
+print(var2[1:5])
---
+++ TutorialPoint/Strings/strings2.py
@@ -0,0 +1,7 @@
+var1 = "Hello World"
+
+print(var1)
+
+var1 = var1[:6] + "Python!"
+
+print(var1)
