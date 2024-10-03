eab6c29d588fe8dd1abd21a96c8fb76cb3f77b41
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 15:17:03 +0530

    MEMBERSHIP OPERATORS

---
+++ TutorialPoint/BasicOperators/membershipoperators.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+list1 = [1, 2, 3, 4, 5]
+
+if a in list1:
+    print('a is available in the given list')
+else:
+    print('a is not available in the given list')
+
+if b not in list1:
+    print('b is not available in the given list')
+else:
+    print('b is available in the given list')
+
+a = 2
+
+if a in list1:
+    print('a is available in the given list')
+else:
+    print('a is not available in the given list')
