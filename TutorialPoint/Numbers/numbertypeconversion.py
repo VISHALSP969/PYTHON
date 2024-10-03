ff9be4eca3b620bfb585f078888d0a3b91c82bac
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 20:53:31 +0530

    NUMBER TYPE CONVERSION

---
+++ TutorialPoint/Numbers/numbertypeconversion.py
@@ -0,0 +1,15 @@
+x = 100
+y = 20.75
+z = 3 + 2j
+
+print(float(x))
+print(complex(x))
+
+print(int(y))
+print(complex(y))
+
+# print(int(z))     # Type Error
+# print(float(z))   # Type Error
+
+p=complex(10,20)
+print(p)
