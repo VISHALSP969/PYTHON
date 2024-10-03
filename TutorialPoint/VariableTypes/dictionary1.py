a4098293fa19905fda7a49753fb535bc0e220e19
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 11:32:35 +0530

    PYTHON DICTIONARIES

---
+++ TutorialPoint/VariableTypes/dictionary1.py
@@ -0,0 +1,20 @@
+dict1 = {}      # empty dictionary
+# add items to dictionary
+dict1['one'] = "This is one"
+dict1[2] = "This is two"
+# prints complete dictionary
+print(dict1)
+
+# prints individual items
+print(dict1['one'])
+print(dict1[2])
+
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+print(tinydict)
+
+print(tinydict.keys())
+print(tinydict.values())
+print(tinydict.items())
+
+tinydict['name']='mathew'
+print(tinydict)
