7eb451454bc4de5fbeb924c13721bbad2771f127
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 15:11:40 +0530

    LOGICAL OPERATORS

---
+++ TutorialPoint/BasicOperators/logicaloperators.py
@@ -0,0 +1,42 @@
+a = 10
+b = 20
+
+print(a and b)
+print(a or b)
+print(not a)
+
+b = 0
+print(a and b)
+print(a or b)
+print(not a)
+
+a = ""
+b = 20
+
+print(a and b)
+print(a or b)
+print(not a)
+
+a = True
+b = True
+print(a and b)
+print(a or b)
+print(not a)
+
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+
+a = False
+b = True
+print(a and b)
+print(a or b)
+print(not a)
+
+a = False
+b = False
+print(a and b)
+print(a or b)
+print(not a)
