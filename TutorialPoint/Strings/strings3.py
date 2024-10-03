a93a1f3f74910ecbf4652edcd33d449f921af26a
VISHALSP969 <vishalsp969@gmail.com>
2024-10-04 03:37:58 +0530

    ESCAPE CHARACTERS

---
+++ TutorialPoint/Strings/strings3.py
@@ -0,0 +1,27 @@
+# escaping single quotes
+print('It\'s a sunny day!')
+
+# escaping double quotes
+print("He said, \"Hello World\"")
+
+# new line
+print("Hello\nWorld")
+
+# tab
+print("Hello\tWorld")
+
+# using backslashes
+print("This is backslash : \\")
+
+# octal and hexa decimal values
+print("\101")
+print("\x41")
+
+# null character
+print("Null character : \0")
+
+# backspace
+print("Helloo\b World")
+
+# carriage return
+print("HELLO\rhello")
