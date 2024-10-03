1c152d9ad1e85a334bd0539a538eab49ae78cbf7
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 10:28:21 +0530

    PYTHON NUMBERS

---
+++ TutorialPoint/VariableTypes/numbers1.py
@@ -0,0 +1,8 @@
+var1 = 1
+var2 = 10
+print(var1)
+print(var2)
+del var1,var2
+# print(var1)       # Name Error
+# print(var2)       # Name Error
+
---
+++ TutorialPoint/VariableTypes/numbers2.py
@@ -0,0 +1,17 @@
+x = 10
+print(x)
+print(type(x))
+
+num = 987654321987654321987654321987654321987654321
+print(num)
+print(type(num))
+
+y = 20.5
+print(y)
+print(type(y))
+
+z = 10 + 7j
+print(z)
+print(type(z))
+
+
