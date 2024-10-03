42a387b430a53b01ea0441506ba784f434ec221d
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 11:01:09 +0530

    PYTHON LISTS

--- TutorialPoint/VariableTypes/lists1.py
+++ TutorialPoint/VariableTypes/lists1.py
@@ -16,7 +16,10 @@ print(list1[:5])
 print(list1[1:4])
 print(list1[2:5])
 
-list2=[1,2,3]
-list3=[4,5,6,7,8,9,10]
-print(list2+list3)
-print(list2*3)
+list2 = [1, 2, 3]
+list3 = [4, 5, 6, 7, 8, 9, 10]
+print(list2 + list3)
+print(list2 * 3)
+
+list2[0] = 0
+print(list2)
