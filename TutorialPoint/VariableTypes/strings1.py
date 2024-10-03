5dfc8eb1559c30b7d6854ff142a19e1fd772dadc
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 10:36:51 +0530

    PYTHON STRINGS

---
+++ TutorialPoint/VariableTypes/strings1.py
@@ -0,0 +1,25 @@
+str1 = 'HELLO WORLD...!'
+str2 = "Hello world...!"
+
+print(str1)
+print(str2)
+
+# accessing using index
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+
+# slice operator
+print(str1[0:5])
+print(str1[6:11])
+print(str1[:])
+print(str1[0:])
+print(str1[:15])
+print(str1[0:15:2])
+print(str1[0:15:3])
+
+print(str1*3)
+
+print(str1+str2)
