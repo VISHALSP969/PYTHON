b8d377a6b2a0be29c4e6b1af6ca7c8499741c5eb
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 17:57:49 +0530

    IF STATEMENTS

---
+++ TutorialPoint/DecisionMaking/elifstatement.py
@@ -0,0 +1,14 @@
+var = 100
+if var == 200:
+    print('Got a true expression value')
+    print(var)
+elif var == 150:
+    print('Got a true expression value')
+    print(var)
+elif var == 100:
+    print('Got a true expression value')
+    print(var)
+else:
+    print('Got a false expression value')
+    print(var)
+print('Good bye!')
