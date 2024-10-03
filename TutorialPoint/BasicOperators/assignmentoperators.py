71c4380874688683593ab71a442dc3c5715e2cab
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 14:57:40 +0530

    ASSIGNMENT OPERATORS

---
+++ TutorialPoint/BasicOperators/assignmentoperators.py
@@ -0,0 +1,30 @@
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print(c)
+
+c += a
+print(c)
+
+c *= a
+print(c)
+
+c /= a
+print(c)
+
+c -= a
+print(c)
+
+c=2
+c%=a
+print(c)
+
+c**=a
+print(c)
+
+c=12
+a=5
+c//=a
+print(c)
