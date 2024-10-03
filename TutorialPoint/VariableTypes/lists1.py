1d630e313e8d3006163165ae38b7a83f4cd171c9
VISHALSP969 <vishalsp969@gmail.com>
2024-10-03 10:48:23 +0530

    PYTHON LISTS

---
+++ TutorialPoint/VariableTypes/lists1.py
@@ -0,0 +1,22 @@
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+
+mixed_list = ['abcd', 786, 2.23, 'john', 70.2]
+print(mixed_list)
+
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+
+print(list1[:])
+print(list1[0:])
+print(list1[:5])
+print(list1[1:4])
+print(list1[2:5])
+
+list2=[1,2,3]
+list3=[4,5,6,7,8,9,10]
+print(list2+list3)
+print(list2*3)
