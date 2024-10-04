0cbc65cfad0fcc8e09be42195c508551f721ea70
VISHALSP969 <vishalsp969@gmail.com>
2024-10-04 17:55:54 +0530

    TUPLE INDEXING AND SLICING

---
+++ TutorialPoint/Tuples/tuple5.py
@@ -0,0 +1,15 @@
+l = ('spam', 'Spam', 'SPAM')
+
+print(l[0])
+print(l[1])
+print(l[2])
+print('----------')
+print(l[-1])
+print(l[-2])
+print(l[-3])
+
+print(l[0:2])
+print(l[1:2])
+
+print(l[-3:-1])
+print(l[-2:-1])
