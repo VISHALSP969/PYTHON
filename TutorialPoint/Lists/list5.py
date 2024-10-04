b4fe93ac3ae3242a0722fef05be4bdd749f77db3
VISHALSP969 <vishalsp969@gmail.com>
2024-10-04 14:07:31 +0530

    LIST BASIC OPERATIONS

---
+++ TutorialPoint/Lists/list5.py
@@ -0,0 +1,20 @@
+list1=[1,2,3,4,5]
+
+# length
+print(len(list1))
+
+# concatenation
+print([1,2,3]+[4,5])
+
+# repetition
+print(["Hi"]*3)
+
+# membership
+print(3 in [1,2,3])
+print(4 in [1,2,3])
+print(3 not in [1,2,3])
+print(4 not in [1,2,3])
+
+# Iteration
+for x in list1:
+    print(x)
