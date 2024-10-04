f3d9b7a78bb2ec69ccc012dc9eb1f674981ffc45
VISHALSP969 <vishalsp969@gmail.com>
2024-10-04 10:33:09 +0530

    MULTI LINE STRINGS

---
+++ TutorialPoint/Strings/multilinestrings.py
@@ -0,0 +1,17 @@
+str1="""
+This is a long string 
+composed of multiple lines
+"""
+
+print(str1)
+
+str2='''
+this is a long string that is made up of
+several lines and non-printable characters such as
+TAB ( ) and they will show up that way when displayed.
+NEWLINEs within the string, whether explicitly given like
+this within the brackets [
+], or just a NEWLINE within
+the variable assignment will also show up.
+'''
+print(str2)
