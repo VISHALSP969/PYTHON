80a2b58ee947446d2be6f01ddea20bef2b4e41d6
VISHALSP969 <vishalsp969@gmail.com>
2024-10-04 14:22:28 +0530

    LIST INDEXING AND SLICING

---
+++ TutorialPoint/Lists/list6.py
@@ -0,0 +1,19 @@
+l = ['spam', 'Spam', 'SPAM']
+
+# indexing
+print(l[0])
+print(l[1])
+print(l[2])
+print('------')
+print(l[-1])
+print(l[-2])
+print(l[-3])
+print('------')
+
+
+# slicing
+print(l[1:])
+print(l[1:2])
+print(l[0:2])
+print(l[-3:-1])
+print(l[-3:-2])
