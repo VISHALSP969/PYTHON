f99cc661c53f1d23c06af4d0b5da9f99734be65e
VISHALSP969 <vishalsp969@gmail.com>
2024-10-05 12:25:23 +0530

    FROM-IMPORT KEYWORD

---
+++ TutorialPoint/Modules/sample2.py
@@ -0,0 +1,7 @@
+from support import print_func      # only print_func() imported
+from support import *       # here all things in module are imported
+
+print_func('John')
+print_func('Zara')
+
+print(sum(10,20))
