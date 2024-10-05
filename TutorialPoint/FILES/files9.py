e0c0b776bbaa948586fbeafc35c5d5331fbebf5c
VISHALSP969 <vishalsp969@gmail.com>
2024-10-05 23:07:37 +0530

    DIRECTORY METHODS

---
+++ TutorialPoint/FILES/files9.py
@@ -0,0 +1,13 @@
+import os
+
+# create directory
+# os.mkdir('sample_dir')
+
+# changing the directory
+# os.chdir('sample_dir')
+
+# getting the present working directory
+print(os.getcwd())
+
+# deleting the directory
+os.rmdir('sample_dir')
