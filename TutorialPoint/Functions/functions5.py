78c5166aaf027f4387822ba8b0763466fd5dffde
VISHALSP969 <vishalsp969@gmail.com>
2024-10-05 10:33:40 +0530

    KEYWORD ARGUMENTS

---
+++ TutorialPoint/Functions/functions5.py
@@ -0,0 +1,8 @@
+# keyword arguments - caller identifies the args by parameter name
+
+def printme(str1):
+    print(str1)
+    return
+
+
+printme(str1="My string")
---
+++ TutorialPoint/Functions/functions6.py
@@ -0,0 +1,6 @@
+def printinfo(name, age):
+    print(name, age)
+    return
+
+
+printinfo(age=20, name='Miki')
