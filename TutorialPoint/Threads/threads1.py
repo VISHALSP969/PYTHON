b8fcc790cc63eb46b69ad40774d63b5ca92fa1d9
VISHALSP969 <vishalsp969@gmail.com>
2024-10-09 09:56:35 +0530

    BASIC THREAD OPERATION

---
+++ TutorialPoint/Threads/threads1.py
@@ -0,0 +1,16 @@
+import threading
+
+
+# function to be executed in a separate thread
+def print_numbers():
+    for i in range(5):
+        print(i)
+
+
+# create a thread
+thread = threading.Thread(target=print_numbers)
+
+# start the thread
+thread.start()
+
+print('Main thread waits until the worker thread is done')
