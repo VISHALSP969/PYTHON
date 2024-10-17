340709a436b02a80ac76292054d334ec23688070
VISHALSP969 <vishalsp969@gmail.com>
2024-10-17 10:40:23 +0530

    Numbers

---
+++ PythonCrashCourse/Numbers/Numbers1.py
@@ -0,0 +1,28 @@
+# integers
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+
+print(3**2)
+print(3**3)
+print(10**6)
+
+print(5/2)
+print(5//2)
+print(5%2)
+
+print(2+3*4)
+print((2+3)*4)
+
+# float
+print(0.1+0.1)
+print(0.2+0.2)
+print(2*0.1)
+print(2*0.2)
+
+
+age=23
+# message="Happy "+age+"rd Birthday"      # Type Error
+message="Happy "+str(age)+"rd Birthday"
+print(message)
---
+++ PythonCrashCourse/Numbers/Numbers2.py
@@ -0,0 +1,7 @@
+print(5 + 3)
+print(13 - 5)
+print(2 * 4)
+print(24 / 3)
+
+favorite_number = 6
+print("Favorite number is " + str(favorite_number))
