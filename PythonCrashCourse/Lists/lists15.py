87825c60b4be3a90b8a72eb11c36294f6956e3fd
VISHALSP969 <vishalsp969@gmail.com>
2024-10-18 12:55:07 +0530

    Slice Operation

---
+++ PythonCrashCourse/Lists/lists15.py
@@ -0,0 +1,24 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:5])                 # print all items
+print(players[0:len(players)])      # prints all items
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[:3])
+print(players[0:])      # prints all items
+print(players[1:])
+print(players[2:])
+print(players[:])       # print all items
+print('------------------------')
+print(players[:0])      # prints empty lis
+print(players[:-1])
+print(players[:-2])
+print(players[-len(players):-1])
+print(players[-3:-1])
+print(players[-4:-2])
+print('------------------------')
+print(players[::2])
+print(players[::3])
+print(players[0:4:2])
+print(players[:-1:2])
+print(players[:-3:2])
