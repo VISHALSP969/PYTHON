763b0b8891c4b19f2d6a06c85946d54ce7b77693
VISHALSP969 <vishalsp969@gmail.com>
2024-10-18 10:55:16 +0530

    List Comprehensions

---
+++ PythonCrashCourse/Lists/lists13.py
@@ -0,0 +1,9 @@
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
+
+cubes = [value ** 3 for value in range(1, 6)]
+print(cubes)
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [value + 3 for value in list1]
+print(list2)
