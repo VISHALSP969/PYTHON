88d0613df9aa6f91f3c42dd751e34849f6a62e24
VISHALSP969 <vishalsp969@gmail.com>
2024-10-18 21:13:42 +0530

    Check equality and in equality

---
+++ PythonCrashCourse/IfStatement/Sample2.py
@@ -0,0 +1,11 @@
+# equality checking
+car = "Bmw"
+print(car == "Bmw")
+print(car == "bmw")
+print(car.lower() == "bmw")
+
+# inequality checking
+car = "Bmw"
+print(car != "bmw")
+print(car != "Bmw")
+print(car.lower() != "bmw")
