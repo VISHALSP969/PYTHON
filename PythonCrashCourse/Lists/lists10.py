655b8eef75297c736855d1bd051e443a3e3369bf
VISHALSP969 <vishalsp969@gmail.com>
2024-10-18 09:46:46 +0530

    Lists

---
+++ PythonCrashCourse/Lists/lists10.py
@@ -0,0 +1,9 @@
+pizzas = ['Paneer Tikka Pizza', 'Tandoori Chicken Pizza', 'Masala Pizza']
+for pizza in pizzas:
+    print("I Like " + pizza)
+print("I really love pizza")
+
+animals = ['Dog', 'Cow', 'Goat']
+for animal in animals:
+    print("A " + animal + " would make a great pet.")
+print("Dog would make a great pet")
