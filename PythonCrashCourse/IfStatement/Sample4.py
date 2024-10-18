d9bb0445a2b059c1dfff2081b85977c714ba805c
VISHALSP969 <vishalsp969@gmail.com>
2024-10-18 23:47:23 +0530

    Checking multiple conditions

---
+++ PythonCrashCourse/IfStatement/Sample4.py
@@ -0,0 +1,13 @@
+age1 = 22
+age2 = 18
+print(age1 >= 21 and age2 >= 21)
+
+age2 = 22
+print(age1 >= 21 and age2 >= 21)
+
+age1 = 22
+age2 = 18
+print(age1 >= 21 or age2 >= 21)
+
+age1 = 18
+print(age1 >= 21 or age2 >= 21)
