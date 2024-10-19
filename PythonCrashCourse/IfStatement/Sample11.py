f661c7fb87a2921bf336db8a614a9f3f5d17f959
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 11:25:36 +0530

    If statement

---
+++ PythonCrashCourse/IfStatement/Sample11.py
@@ -0,0 +1,15 @@
+alien_color = "green"
+
+if alien_color == "green":
+    print("Earned 5 points")
+
+if alien_color == "green":
+    print("Earned 5 points")
+else:
+    print("You fails")
+
+alien_color = "green"
+if alien_color == "green":
+    print("You earned 5 points")
+else:
+    print("You earned 10 points")
