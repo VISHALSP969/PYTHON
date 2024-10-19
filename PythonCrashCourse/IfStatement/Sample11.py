5654ccc0506c9a0251f2123e96842a1de2e44e85
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 12:04:46 +0530

    Tf statement

--- PythonCrashCourse/IfStatement/Sample11.py
+++ PythonCrashCourse/IfStatement/Sample11.py
@@ -13,3 +13,37 @@ if alien_color == "green":
     print("You earned 5 points")
 else:
     print("You earned 10 points")
+
+alien_color = "red"
+
+if alien_color == 'green':
+    print("You earned 5 points")
+elif alien_color == "yellow":
+    print("You earned 10 points")
+else:
+    print("You earned 15 points")
+
+age = 30
+if age < 2:
+    print('Person is a baby.')
+elif age < 4:
+    print("Person is a toddler")
+elif age < 13:
+    print("Person is a kid")
+elif age < 20:
+    print('Person is a teenager')
+elif age < 65:
+    print("Person is a an adult")
+else:
+    print("Person is an elder")
+
+fruits=['apple','banana','cherry','orange','kiwi']
+
+print('jackfruit' in fruits)
+print('orange' in fruits)
+print('tomato' in fruits)
+print('apple' in fruits)
+print('jackfruit' not in fruits)
+print('orange' not in fruits)
+print('tomato' not in fruits)
+print('apple' not in fruits)
