94fae61e61c402135c5af5b6a30fe9c325747c25
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 11:04:37 +0530

    Testing multiple conditions

---
+++ PythonCrashCourse/IfStatement/Sample10.py
@@ -0,0 +1,15 @@
+requested_toppings=['mushroom','extra cheese']
+
+if 'mushroom' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print('adding pepperoni')
+if 'extra cheese' in requested_toppings:
+    print("Add extra cheese.")
+
+if 'mushroom' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print('adding pepperoni')
+elif 'extra cheese' in requested_toppings:
+    print("Add extra cheese.")
