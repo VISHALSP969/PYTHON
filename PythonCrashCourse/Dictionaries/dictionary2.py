e632f7dc87338cc74a18d043c505d3651d269e56
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 20:22:48 +0530

    Add new key-value pair

--- PythonCrashCourse/Dictionaries/dictionary2.py
+++ PythonCrashCourse/Dictionaries/dictionary2.py
@@ -5,3 +5,10 @@ print(alien_0)
 alien_0['x-position'] = 0
 alien_0['y-position'] = 25
 print(alien_0)
+
+alien_1 = {}
+print(alien_1)
+
+alien_1['color'] = 'green'
+alien_1['points'] = 5
+print(alien_1)
