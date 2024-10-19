21897d224c09d9cad9a0213f5a724982ec1a2fe9
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 20:08:42 +0530

    Dictionary

---
+++ PythonCrashCourse/Dictionaries/dictionary1.py
@@ -0,0 +1,13 @@
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+# simplest dictionary - one key value pair
+alien_1 = {'color': 'green'}
+
+# Accessing values in dictionary
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print('You just earned ' + str(new_points) + ' points.')
+
