bda52a8fa5ab6645aad7403795db5b6ca2d937f5
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 10:14:54 +0530

    Checking conditions

---
+++ PythonCrashCourse/IfStatement/Sample6.py
@@ -0,0 +1,60 @@
+car = 'subaru'
+print("Is car=='subaru'?I predict True.")
+print(car == 'subaru')
+print("Is car=='audi'? I predict False.")
+print(car == 'audi')
+
+car = "bmw"
+print("Is car=='bmw'?I predict True.")
+print(car == 'bmw')
+print("Is car=='audi'? I predict False.")
+print(car == 'audi')
+
+car = "ford"
+print("Is car=='ford'?I predict True.")
+print(car == 'ford')
+print("Is car=='audi'? I predict False.")
+print(car == 'audi')
+
+car = 'mercedes'
+print("Is car=='mercedes'?I predict True.")
+print(car == 'mercedes')
+print("Is car=='audi'? I predict False.")
+print(car == 'audi')
+
+car = 'ferrari'
+print("Is car=='ferrari'?I predict True.")
+print(car == 'ferrari')
+print("Is car=='audi'? I predict False.")
+print(car == 'audi')
+
+str1 = "welcome"
+str2 = "bye"
+print(str1 == str2)
+print(str1 != str2)
+print(str1 == "WELCOME")
+print(str1.upper() == "WELCOME")
+print(str1 == "welcome" and str2 == "bye")
+print(str1 == "welcome" and str2 == "BYE")
+print(str1 == "welcome" or str2 == "bye")
+print(str1 == "welcome" or str2 == "BYE")
+print(str1 == "WELCOME" or str2 == "BYE")
+
+str1 = "WELCOME"
+print(str1 == "welcome")
+print(str1 != "welcome")
+print(str1.lower() == "welcome")
+print(str1 == "WELCOME")
+
+num1 = 10
+num2 = 20
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 <= num2)
+
+numbers = [1, 2, 3, 4, 5]
+print(10 in numbers)
+print(5 in numbers)
+print(10 not in numbers)
+print(5 not in numbers)
