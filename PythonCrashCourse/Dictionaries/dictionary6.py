4447aaced6c8a9b36f16cdc9921fd9f2049c8e31
VISHALSP969 <vishalsp969@gmail.com>
2024-10-19 22:29:08 +0530

    Dictionary

---
+++ PythonCrashCourse/Dictionaries/dictionary6.py
@@ -0,0 +1,14 @@
+person = {'first_name': 'John', 'last_name': 'Doe', 'age': 28, 'city': 'California'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+
+favorite_number = {'kroos': 8, 'modrich': 10, 'benzema': 9, 'cristiano': 7, 'bale': 11}
+print(favorite_number)
+favorite_number['ramos'] = 4
+print(favorite_number)
+
+glossary = {'if': 'conditional statement', 'break': 'loop control statement', 'list': 'collection datatype',
+            'in': 'membership operator', 'int': 'primitive datatype'}
+print(glossary)
