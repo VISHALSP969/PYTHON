0509e50e4dbaa0d9a813bf09e6b30260f58e549a
VISHALSP969 <vishalsp969@gmail.com>
2024-10-20 20:50:37 +0530

    While loop

---
+++ PythonCrashCourse/UserInputAndWhileLoops/while5.py
@@ -0,0 +1,28 @@
+# topping = ""
+# while topping != 'quit':
+#     prompt = "Enter the topping:"
+#     prompt += "\n(Type 'quit' to finish.)"
+#     topping = input(prompt)
+#
+# while True:
+#     age = int(input("Enter the age : "))
+#     if age < 3:
+#         print("Free entry")
+#     elif age < 12:
+#         print("Ticket rate : $10")
+#     else:
+#         print("Ticket Rate : $15")
+#
+# active = True
+# while active:
+#     prompt = "Enter the topping you want : "
+#     prompt += "\n(Type stop to finish.)"
+#     topping = input(prompt)
+#     if topping == 'stop':
+#         active = False
+#         break
+#     else:
+#         print(topping)
+#
+# while True:
+#     print("Inside infinite loop")
