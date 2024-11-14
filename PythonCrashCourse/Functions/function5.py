80d21ffa0bf4221f999b35b5afc81836da3ff0b8
VISHALSP969 <vishalsp969@gmail.com>
2024-11-14 15:53:03 +0530

    Positional arguments

---
+++ PythonCrashCourse/Functions/function5.py
@@ -0,0 +1,6 @@
+def describe_pet(animal_type,pet_name):
+    print("\bI have a "+animal_type+".")
+    print("My "+animal_type+"'s name is "+pet_name.title()+".")
+
+describe_pet("Hamster","Harry")
+describe_pet("Dog","Willie")
