8c025b13e000a9f605ec18875c40aa7f683fe561
VISHALSP969 <vishalsp969@gmail.com>
2024-11-25 00:45:02 +0530

    Method overriding

---
+++ PythonCrashCourse/classes/class18.py
@@ -0,0 +1,11 @@
+class Parent:
+    def show(self):
+        print("Inside Parent")
+
+class Child(Parent):
+    def show(self):
+        Parent.show(self)
+        print("Inside Child")
+
+obj=Child()
+obj.show()
