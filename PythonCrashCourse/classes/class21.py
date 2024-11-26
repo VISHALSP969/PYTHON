fd837d570faa00ea52c4c322e2b35c7ab0042a75
VISHALSP969 <vishalsp969@gmail.com>
2024-11-26 11:52:56 +0530

    Instance as attribute

--- PythonCrashCourse/classes/class21.py
+++ PythonCrashCourse/classes/class21.py
@@ -33,9 +33,6 @@ class ElectricCar(Car):
         super().__init__(make,model,year)
         self.battery=Battery()
 
-    def describe_battery(self):
-        print("This car has a "+str(self.battery_size)+"-kWh battery.git")
-
 my_tesla=ElectricCar('tesla','model s',2016)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
