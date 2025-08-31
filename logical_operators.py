18677e32d0b4f697591323f0cc09db64905a41ef
VISHALSP969 <vishalsp969@gmail.com>
2025-08-31 12:28:44 +0530

    Logical Operators

---
+++ logical_operators.py
@@ -0,0 +1,12 @@
+print(True and False)
+print(True or False)
+print(not(True) and False)
+print(not(True and False))
+
+print((10<0) and (10>2))
+print((10<0) or (10>2))
+print(not((10<2) or (10>2)))
+print(not(10<0 or 10>2))
+
+print(1>2 and  9>6)
+print(3>2 or 8<4)
