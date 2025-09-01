41e509f121eccded804eedb1ba0c9b6010335cbb
VISHALSP969 <vishalsp969@gmail.com>
2025-09-01 21:22:23 +0530

    Basic Datatypes

---
+++ basic_datatypes.py
@@ -0,0 +1,23 @@
+a=10
+print(a)
+pi=3.14
+print(pi)
+comp=3+2j
+print(comp)
+
+boolean=True
+print(boolean)
+boolean=False
+print(boolean)
+
+str1='This is single quote string'
+print(str1)
+str2="This is double quote string"
+print(str2)
+str3="""This is
+multiline
+string"""
+print(str3)
+
+x=None
+print(x)
