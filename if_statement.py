a447e7a403417263e42ef258688d9f346d13fdfc
VISHALSP969 <vishalsp969@gmail.com>
2025-09-03 13:44:52 +0530

    If Statement

---
+++ if_statement.py
@@ -0,0 +1,12 @@
+if 20>10:
+    print("20 is greater than 10")
+
+# Prints a  message if the number is positive
+number=int(input("Enter a  number : "))
+if number>=0:
+    print("The number entered by the user is a positive number")
+
+weight=int(input("How many pounds does your suitcase weight ? "))
+if weight>50:
+    print("There is a $25 surcharge for heavy luggage")
+    print("Thank you")
