8f479e0eddc52c4598973f44a404f2a04d170a68
VISHALSP969 <vishalsp969@gmail.com>
2025-09-03 13:55:45 +0530

    If-Else Statement

---
+++ if_else_statement.py
@@ -0,0 +1,14 @@
+# Checking even or odd
+number=int(input("Enter a number : "))
+if number%2==0:
+    print(f"{number} is Even number")
+else:
+    print(f"{number} is Odd number")
+
+# Find greater of two numbers
+number1=int(input("Enter the first number : "))
+number2=int(input("Enter the second number : "))
+if number1>number2:
+    print(f"{number1} is greater than {number2}")
+else:
+    print(f"{number2} is greater than {number1}")
